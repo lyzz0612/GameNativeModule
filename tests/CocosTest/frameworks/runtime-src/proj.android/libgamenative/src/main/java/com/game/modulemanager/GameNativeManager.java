40bbb15dc400556e54ac02906cf93205f6410b26
lingyun <lingyun@yangeit.com>
2018-11-15 18:42:50 +0800

    new test project

---
+++ tests/CocosTest/frameworks/runtime-src/proj.android/libgamenative/src/main/java/com/game/modulemanager/GameNativeManager.java
@@ -0,0 +1,34 @@
+package com.game.modulemanager;
+
+import android.app.Activity;
+import android.content.Intent;
+
+/**
+ * Created by lyzz0612 on 2018/11/15 0015.
+ */
+
+public class GameNativeManager {
+    static Activity _activity;
+
+    public static void onCreate(Activity acitivity) {
+        _activity = acitivity;
+
+    }
+
+    public static void onWindowFocusChanged(boolean hasFocus) {
+
+    }
+    public static void onResume() {
+
+    }
+    public static void onPause() {
+
+    }
+    public static void onDestroy() {
+
+    }
+    public static void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    }
+
+}
